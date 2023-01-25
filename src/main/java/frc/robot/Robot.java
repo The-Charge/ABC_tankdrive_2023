337d9e02e82d6432fe13d3d669649e687c99a307
abcurrie <abcurrie@gmail.com>
2023-01-25 10:39:50 -0500

    Added Follower motors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,7 +11,8 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 /**
- * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
+ * This is a demo program showing the use of the DifferentialDrive class,
+ * specifically it contains
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
@@ -19,27 +20,32 @@ public class Robot extends TimedRobot {
   private Joystick m_leftStick;
   private Joystick m_rightStick;
 
-private WPI_TalonSRX leftFrontMotor;
-//private WPI_TalonFX leftRearMotor;
-//private MotorControllerGroup left;
-private WPI_TalonSRX rightFrontMotor;
-//private WPI_TalonFX rightRearMotor;
-//private MotorControllerGroup right;
-  
-  //private final MotorController leftFrontMotor = new WPI_TalonSRX(1);
-  //private final MotorController righFronttMotor = new WPI_TalonSRX(7);
+  private WPI_TalonSRX leftFrontMotor;
+  private WPI_TalonSRX leftRearMotor;
+
+  // private MotorControllerGroup left;
+  private WPI_TalonSRX rightFrontMotor;
+  private WPI_TalonSRX rightRearMotor;
+  // private MotorControllerGroup right;
+
+  // private final MotorController leftFrontMotor = new WPI_TalonSRX(1);
+  // private final MotorController righFronttMotor = new WPI_TalonSRX(7);
 
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    
-    
-   leftFrontMotor = new WPI_TalonSRX(1);
-   rightFrontMotor = new WPI_TalonSRX(7);
-    
+
+    leftFrontMotor = new WPI_TalonSRX(1);
+    rightFrontMotor = new WPI_TalonSRX(7);
+    leftRearMotor = new WPI_TalonSRX(2);
+    rightRearMotor = new WPI_TalonSRX(8);
+
     rightFrontMotor.setInverted(true);
+
+    leftRearMotor.follow(leftFrontMotor);
+    rightRearMotor.follow(rightFrontMotor);
 
     m_myRobot = new DifferentialDrive(leftFrontMotor, rightFrontMotor);
     m_leftStick = new Joystick(0);
